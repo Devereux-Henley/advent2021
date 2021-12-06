fa01071a4b0c161b8298cfed62cb5ca3dcbce8e0
Devereux Henley <devereux.henley@artisantechgroup.com>
2021-12-06 16:37:35 -0600

    Added part 2-1 solution.

---
+++ src/main/two/solution.clj
@@ -0,0 +1,55 @@
+(ns two.solution)
+
+(defprotocol INavigable
+  (navigate [this instruction] "Navigates to a new position based on an inbound instruction.")
+  (distance-from-start [this] "Returns the distance of the navigable from it's start position."))
+
+(defrecord Submarine [depth position]
+  INavigable
+  (navigate [this {:keys [direction magnitude]}]
+    (case direction
+      :forward (assoc this :position (+ position magnitude))
+      :backward (assoc this :position (- position magnitude))
+      :up (assoc this :depth (- depth magnitude))
+      :down (assoc this :depth (+ depth magnitude))
+      ))
+  (distance-from-start [this]
+    (* position depth)))
+
+(defn to-instruction [instruction]
+  (let [[direction magnitude] (clojure.string/split instruction #" ")]
+    {:direction (keyword direction) :magnitude (Integer/parseInt magnitude)}
+    )
+  )
+
+(defn drive-submarine
+  [instruction])
+
+(defn solve-part-one
+  []
+  (->>
+   (slurp "resources/2-1.txt")
+   (clojure.string/split-lines)
+   (map to-instruction)
+   (reduce (fn [submarine instruction] (navigate submarine instruction)) (->Submarine 0 0))
+   distance-from-start
+   )
+  )
+
+(comment
+  (->>
+   [
+    {:direction :forward :magnitude 5}
+    {:direction :down :magnitude 5}
+    {:direction :forward :magnitude 8}
+    {:direction :up :magnitude 3}
+    {:direction :down :magnitude 8}
+    {:direction :forward :magnitude 2}
+    ]
+   (reduce (fn [submarine instruction] (navigate submarine instruction)) (->Submarine 0 0))
+   distance-from-start
+   )
+
+  (solve-part-one)
+
+  )
